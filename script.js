2767e6f3105d6e6f519e6fdae658a0ecf3ff1b9b
umeshgt <79749810+Umeshgt@users.noreply.github.com>
2022-08-26 10:27:11 +0530

    final

--- script.js
+++ script.js
@@ -28,8 +28,8 @@ class Enemy{
         this.angleSpeed = Math.random() * 0.5 + 0.5
     }
     update(){
-        this.x = (canvas.width/2) * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2)
-        this.y = (canvas.height/2) * Math.cos(this.angle * Math.PI/180) + (canvas.height/2 - this.width/2)
+        this.x =  canvas.width/2 */*is the amplitude of wave from fixed x*/  Math.sin(this.angle * Math.PI/90) /*is the angle verticle change*/  + (canvas.width/2 - this.width/2) //takes the fixed x in the middle of the canvas
+        this.y = canvas.height/2 * Math.cos(this.angle * Math.PI/180) + (canvas.height/2 - this.width/2)
         
         this.angle += this.angleSpeed
         
